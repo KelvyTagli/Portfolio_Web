584577084efe8e7ff6abebf23adf4fcd2e9e35e7
Kelvy Tagliacolli <kelvytagliacolli@hotmail.com.br>
2024-07-18 23:23:07 -0300

    feat:hard skills page

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,13 +1,28 @@
-import { Container, About_} from "./styles"
-
+import { Container, About_, HardSkills} from "./styles"
+import javascript from '../../assets/JavaScript-logo.png'
 export function About() {
     return (
         <Container>
             <About_>
-                <img src="" alt="" />
+                <img src={javascript} alt="" />
                 <h1>Kelvy Tagliacolli</h1>
                 <p>Meu nome é Kelvy Tagliacolli, sou de São Paulo e sou formado em Análise e Desenvolvimento de Sistemas pela UNIP (Universidade Paulista).</p>
             </About_>
+                <h1>Hard Skills</h1>
+            <HardSkills>
+                <div className="skills">
+                    
+                </div>
+                <div className="skills">
+                    
+                </div>
+                <div className="skills">
+                    
+                </div>
+                <div className="skills">
+                    
+                </div>
+            </HardSkills>
         </Container>
     )
 }
--- src/Pages/About/styles.js
+++ src/Pages/About/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
-    height: 89vh;
+    height: 100vh;
 
     display: flex;
     flex-direction: column;
@@ -18,16 +18,41 @@ export const About_ = styled.div `
     
     display: flex;
     flex-direction: column;
-    text-align: center;
+    align-items: center;
 
     gap: 2rem;
 
+    > img {
+        height: 167px;
+
+        border: 4px solid ${({theme}) => theme.COLORS.GRAY_100};
+        border-radius: 20%;
+
+        padding: 1rem;
+    }
+
     > h1 {
         font-size: 54px;
+        font-weight: 500;
     }
 
     > p {
-        font-size: 18px;
-        letter-spacing: 2px;
+        font-size: 14px;
+        font-weight: 300;
+    }
+`
+
+export const HardSkills = styled.div `
+    width: 1032px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    > .skills {
+        width: 212px;
+        height: 98px;
+
+        border: 1px solid white;
     }
 `
