c970860625ba338656e6c6a6597e64aae6c04b75
Kelvy Tagliacolli <kelvytagliacolli@hotmail.com.br>
2024-07-26 22:34:18 -0300

    test vite deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { ThemeProvider } from 'styled-components'
-import Header  from './Components/Header'
+import {Header}  from './Components/Header'
 
 import theme from './styles/theme.js'
 
