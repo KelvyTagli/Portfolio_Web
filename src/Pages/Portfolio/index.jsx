def5503b7ea0935ce24aedf407a9595d860c1c8c
Kelvy Tagliacolli <kelvytagliacolli@hotmail.com.br>
2024-08-25 12:29:10 -0300

    add projeto

--- src/Pages/Portfolio/index.jsx
+++ src/Pages/Portfolio/index.jsx
@@ -1,6 +1,7 @@
 import { Container, Title } from "./styles";
 import { Card } from "../../Components/Cards";
 import Cover from "../../assets/Cover.png"
+import Food from "../../assets/Capa.png"
 import { ArrowCircleUp } from "phosphor-react";
 
 export function Portfolio() {
@@ -19,6 +20,13 @@ export function Portfolio() {
                 title={"TypeWeather"}
                 year={2023}
                 description={"O TypeWeather é uma aplicação web para consultar dados meteorológicos e previsão do tempo."}
+            />
+            <Card
+                href={"https://github.com/KelvyTagli/Rocket_Food"}
+                cover={Food}
+                title={"Rocket Food"}
+                year={2023}
+                description={"O food explorer é um projeto desenvolvido como parte do desafio final do programa Explorer. Trata-se de uma aplicação web voltada para a experiência gastronômica, onde os usuários podem explorar uma variedade de pratos e realizar pedidos de maneira prática e intuitiva."}
             />
         </Container>
     )
